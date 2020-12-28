d8697e0e3985f03f5f92eabe23149b2765a7d154
Restyled.io <commits@restyled.io>
2020-12-28 17:23:39 +0000

    Restyled by black

--- redash_toolbelt/examples/refresh_query.py
+++ redash_toolbelt/examples/refresh_query.py
@@ -7,42 +7,50 @@ import json
 
 def poll_job(s, redash_url, job):
     # TODO: add timeout
-    while job['status'] not in (3,4):
-        response = s.get('{}/api/jobs/{}'.format(redash_url, job['id']))
-        job = response.json()['job']
+    while job["status"] not in (3, 4):
+        response = s.get("{}/api/jobs/{}".format(redash_url, job["id"]))
+        job = response.json()["job"]
         time.sleep(1)
 
-    if job['status'] == 3:
-        return job['query_result_id']
-    
+    if job["status"] == 3:
+        return job["query_result_id"]
+
     return None
 
 
 def get_fresh_query_result(redash_url, query_id, api_key, params):
     s = requests.Session()
-    s.headers.update({'Authorization': 'Key {}'.format(api_key)})
+    s.headers.update({"Authorization": "Key {}".format(api_key)})
 
     payload = dict(max_age=0, parameters=params)
 
-    response = s.post('{}/api/queries/{}/results'.format(redash_url, query_id), data=json.dumps(payload))
+    response = s.post(
+        "{}/api/queries/{}/results".format(redash_url, query_id),
+        data=json.dumps(payload),
+    )
 
     if response.status_code != 200:
-        raise Exception('Refresh failed.')
+        raise Exception("Refresh failed.")
 
-    result_id = poll_job(s, redash_url, response.json()['job'])
+    result_id = poll_job(s, redash_url, response.json()["job"])
 
     if result_id:
-        response = s.get('{}/api/queries/{}/results/{}.json'.format(redash_url, query_id, result_id))
+        response = s.get(
+            "{}/api/queries/{}/results/{}.json".format(redash_url, query_id, result_id)
+        )
         if response.status_code != 200:
-            raise Exception('Failed getting results.')
+            raise Exception("Failed getting results.")
     else:
-        raise Exception('Query execution failed.')
+        raise Exception("Query execution failed.")
 
-    return response.json()['query_result']['data']['rows']
+    return response.json()["query_result"]["data"]["rows"]
 
-if __name__ == '__main__':
-    params = {'some_parameter': 1}
+
+if __name__ == "__main__":
+    params = {"some_parameter": 1}
     query_id = 1234
     # Need to use a *user API key* here (and not a query API key).
-    api_key = '...'
-    pprint(get_fresh_query_result('https://app.redash.io/acme', query_id, api_key, params))
+    api_key = "..."
+    pprint(
+        get_fresh_query_result("https://app.redash.io/acme", query_id, api_key, params)
+    )
