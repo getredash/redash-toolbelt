c9a33a24f24823c729d07a80407eff623ceb0c5c
Jesse <jesse.whitehouse@databricks.com>
2021-03-03 09:36:45 -0600

    Example script: Duplicate a dashboard and its queries (#30)
    * Adds a duplicate_query method.
    * Applies black formatting to client.py
    * Bump version number for new CLI command.

--- redash_toolbelt/client.py
+++ redash_toolbelt/client.py
@@ -5,69 +5,91 @@ class Redash(object):
     def __init__(self, redash_url, api_key):
         self.redash_url = redash_url
         self.session = requests.Session()
-        self.session.headers.update({'Authorization': 'Key {}'.format(api_key)})
+        self.session.headers.update({"Authorization": "Key {}".format(api_key)})
 
     def test_credentials(self):
         try:
-            response = self._get('api/session')
+            response = self._get("api/session")
             return True
         except requests.exceptions.HTTPError:
             return False
 
     def queries(self, page=1, page_size=25):
         """GET api/queries"""
-        return self._get('api/queries', params=dict(page=page, page_size=page_size)).json()
+        return self._get(
+            "api/queries", params=dict(page=page, page_size=page_size)
+        ).json()
 
     def dashboards(self, page=1, page_size=25):
         """GET api/dashboards"""
-        return self._get('api/dashboards', params=dict(page=page, page_size=page_size)).json()
+        return self._get(
+            "api/dashboards", params=dict(page=page, page_size=page_size)
+        ).json()
 
     def dashboard(self, slug):
         """GET api/dashboards/{slug}"""
-        return self._get('api/dashboards/{}'.format(slug)).json()
+        return self._get("api/dashboards/{}".format(slug)).json()
 
     def create_dashboard(self, name):
-        return self._post('api/dashboards', json={'name': name}).json()
+        return self._post("api/dashboards", json={"name": name}).json()
 
     def update_dashboard(self, dashboard_id, properties):
-        return self._post('api/dashboards/{}'.format(dashboard_id), json=properties).json()
+        return self._post(
+            "api/dashboards/{}".format(dashboard_id), json=properties
+        ).json()
 
     def create_widget(self, dashboard_id, visualization_id, text, options):
         data = {
-            'dashboard_id': dashboard_id,
-            'visualization_id': visualization_id,
-            'text': text,
-            'options': options,
-            'width': 1,
+            "dashboard_id": dashboard_id,
+            "visualization_id": visualization_id,
+            "text": text,
+            "options": options,
+            "width": 1,
         }
-        return self._post('api/widgets', json=data)
+        return self._post("api/widgets", json=data)
 
     def duplicate_dashboard(self, slug, new_name=None):
         current_dashboard = self.dashboard(slug)
 
         if new_name is None:
-            new_name = u'Copy of: {}'.format(current_dashboard['name'])
+            new_name = "Copy of: {}".format(current_dashboard["name"])
 
         new_dashboard = self.create_dashboard(new_name)
-        if current_dashboard['tags']:
-            self.update_dashboard(new_dashboard['id'], {'tags': current_dashboard['tags']})
+        if current_dashboard["tags"]:
+            self.update_dashboard(
+                new_dashboard["id"], {"tags": current_dashboard["tags"]}
+            )
 
-        for widget in current_dashboard['widgets']:
+        for widget in current_dashboard["widgets"]:
             visualization_id = None
-            if 'visualization' in widget:
-                visualization_id = widget['visualization']['id']
-            self.create_widget(new_dashboard['id'], visualization_id, widget['text'], widget['options'])
+            if "visualization" in widget:
+                visualization_id = widget["visualization"]["id"]
+            self.create_widget(
+                new_dashboard["id"], visualization_id, widget["text"], widget["options"]
+            )
 
         return new_dashboard
+
+    def duplicate_query(self, query_id, new_name=None):
+
+        response = self._post(f"api/queries/{query_id}/fork")
+        new_query = response.json()
+
+        if not new_name:
+            return new_query
+
+        new_query["name"] = new_name
+
+        return self.update_query(new_query.get("id"), new_query).json()
 
     def scheduled_queries(self):
         """Loads all queries and returns only the scheduled ones."""
         queries = self.paginate(self.queries)
-        return filter(lambda query: query['schedule'] is not None, queries)
+        return filter(lambda query: query["schedule"] is not None, queries)
 
     def update_query(self, query_id, data):
         """POST /api/queries/{query_id} with the provided data object."""
-        path = 'api/queries/{}'.format(query_id)
+        path = "api/queries/{}".format(query_id)
         return self._post(path, json=data)
 
     def paginate(self, resource):
@@ -79,23 +101,23 @@ class Redash(object):
         items = []
 
         while not stop_loading:
-            response =  resource(page=page, page_size=page_size)
+            response = resource(page=page, page_size=page_size)
 
-            items += response['results']
+            items += response["results"]
             page += 1
 
-            stop_loading = response['page'] * response['page_size'] >= response['count']
+            stop_loading = response["page"] * response["page_size"] >= response["count"]
 
         return items
 
     def _get(self, path, **kwargs):
-        return self._request('GET', path, **kwargs)
+        return self._request("GET", path, **kwargs)
 
     def _post(self, path, **kwargs):
-        return self._request('POST', path, **kwargs)
+        return self._request("POST", path, **kwargs)
 
     def _request(self, method, path, **kwargs):
-        url = '{}/{}'.format(self.redash_url, path)
+        url = "{}/{}".format(self.redash_url, path)
         response = self.session.request(method, url, **kwargs)
         response.raise_for_status()
         return response
---
+++ redash_toolbelt/examples/clone_dashboard_and_queries.py
@@ -0,0 +1,84 @@
+import click
+
+from redash_toolbelt import Redash
+
+
+def duplicate(client, slug, prefix=None):
+    """Creates a blank dashboard, duplicates the original's queries,
+    and populates it with fresh widgets that mirror the original widgets.
+    """
+
+    # Copped this logic directly from Redash.duplicate_dashboard
+    current_dashboard = client.dashboard(slug)
+    new_dash_name = "Copy of: {}".format(current_dashboard["name"])
+    new_dashboard = client.create_dashboard(new_dash_name)
+
+    if current_dashboard["tags"]:
+        client.update_dashboard(
+            new_dashboard["id"], {"tags": current_dashboard["tags"]}
+        )
+
+    # Widgets can hold text boxes or visualizations. Filter out text boxes.
+    # I use a dictionary here because it de-duplicates query IDs
+    queries_to_duplicate = {
+        widget["visualization"]["query"]["id"]: widget["visualization"]["query"]
+        for widget in current_dashboard.get("widgets", [])
+        if "visualization" in widget
+    }
+
+    # Fetch full query details for the old query IDs
+    # Duplicate the query and store the result
+    old_vs_new_query_pairs = [
+        {
+            "old_query": client._get(f"api/queries/{old_query.get('id')}").json(),
+            "new_query": client.duplicate_query(
+                old_query.get("id"), new_name=" ".join([prefix + old_query.get("name")])
+            ),
+        }
+        for old_query in queries_to_duplicate.values()
+    ]
+
+    # Compare old visualizations to new ones
+    # Create a mapping of old visualization IDs to new ones
+    old_viz_vs_new_viz = {
+        old_viz.get("id"): new_viz.get("id")
+        for pair in old_vs_new_query_pairs
+        for old_viz in pair["old_query"].get("visualizations")
+        for new_viz in pair["new_query"].get("visualizations")
+        if old_viz.get("options") == new_viz.get("options")
+    }
+
+    # This is a version of the same logic from Redash.duplicate_dashboard
+    # But it substitutes in the new visualiation ID pointing at the copied query.
+    for widget in current_dashboard["widgets"]:
+        visualization_id = None
+        if "visualization" in widget:
+            visualization_id = old_viz_vs_new_viz.get(widget["visualization"]["id"])
+        client.create_widget(
+            new_dashboard["id"], visualization_id, widget["text"], widget["options"]
+        )
+
+    return new_dashboard
+
+
+@click.command()
+@click.argument("redash_host")
+@click.argument("slug")
+@click.argument("prefix")
+@click.option(
+    "--api-key",
+    "api_key",
+    envvar="REDASH_API_KEY",
+    show_envvar=True,
+    prompt="API Key",
+    help="User API Key",
+)
+def main(redash_host, slug, prefix="", api_key):
+    """Calls the duplicate function using Click commands"""
+
+    client = Redash(redash_host, api_key)
+    duplicate(client, slug, prefix)
+
+
+if __name__ == "__main__":
+    main()
