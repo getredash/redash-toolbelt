4ab7911472164bd236bb9f5b3bd94882ed6d0a32
Takuya Arita <takuya.arita@gmail.com>
2021-03-04 01:46:23 +0900

    Update example of export queries. (#29)
    * Update example of export queries.
    * Add to scripts and bump version number.
    
    Co-authored-by: Jesse Whitehouse <jesse@whitehouse.dev>

--- redash_toolbelt/examples/query_export.py
+++ redash_toolbelt/examples/query_export.py
@@ -1,49 +1,46 @@
 import click
 import requests
+from redash_toolbelt.client import Redash
 
 template = u"""/*
 Name: {name}
 Data source: {data_source}
 Created By: {created_by}
-Last Update At: {last_updated_at}
+Last Updated At: {last_updated_at}
 */
 {query}"""
 
 
-def get_queries(url, api_key):
-    queries = [] 
-    headers = {'Authorization': 'Key {}'.format(api_key)}
-    path = "{}/api/queries".format(url)
-    has_more = True
-    page = 1
-    while has_more:
-        response = requests.get(path, headers=headers, params={'page': page}).json()
-        queries.extend(response['results'])
-        has_more = page * response['page_size'] + 1 <= response['count']
-        page += 1
-
-    return queries
-
-
 def save_queries(queries):
     for query in queries:
-        filename = 'query_{}.sql'.format(query['id'])
-        with open(filename, 'w') as f:
-            content = template.format(name=query['name'],
-                       data_source=query['data_source_id'],
-                       created_by=query['user']['name'],
-                       last_updated_at=query['updated_at'],
-                       query=query['query'])
+        filename = "query_{}.sql".format(query["id"])
+        with open(filename, "w") as f:
+            content = template.format(
+                name=query["name"],
+                data_source=query["data_source_id"],
+                created_by=query["user"]["name"],
+                last_updated_at=query["updated_at"],
+                query=query["query"],
+            )
             f.write(content)
 
 
 @click.command()
-@click.option('--redash-url')
-@click.option('--api-key', help="API Key")
+@click.argument("redash_url")
+@click.option(
+    "--api-key",
+    "api_key",
+    required=True,
+    envvar="REDASH_API_KEY",
+    show_envvar=True,
+    prompt="API Key",
+    help="User API Key",
+)
 def main(redash_url, api_key):
-    queries = get_queries(redash_url, api_key)      
+    redash = Redash(redash_url, api_key)
+    queries = redash.paginate(redash.queries)
     save_queries(queries)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
